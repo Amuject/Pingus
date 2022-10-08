28ad2fa299ddb97bcca54145789a73969e6323fb
Wany <sung@wany.io>
2022-10-08 18:55:40 +0900

    fix error handle

--- example/test.mjs
+++ example/test.mjs
@@ -1,1 +1,28 @@
 import pingus from 'pingus';
+
+const host = 'wany.io';
+
+//pingus.icmp({ host: host }).then(console.log).catch(console.warn);
+
+//pingus.tcp({ host: host }).then(console.log).catch(console.warn);
+
+//pingus.udp({ host: host }).then(console.log).catch(console.warn);
+
+pingus
+  .traceroute({ host: host, timeout: 500 })
+  .then(console.log)
+  .catch(console.warn);
+/*pingus
+  .tcpscan({ host: host, ports: '@' })
+  .then((res) => {
+    delete res.names;
+    console.log(res);
+  })
+  .catch(console.warn);*/
+/*pingus
+  .udpscan({ host: host, ports: '@' })
+  .then((res) => {
+    delete res.names;
+    console.log(res);
+  })
+  .catch(console.warn);*/
--- src/ping.mjs
+++ src/ping.mjs
@@ -14,6 +14,8 @@ const knownPorts = JSON.parse(
   fs.readFileSync(path.resolve(___dirname, '../data/known-ports.json'))
 );
 
+import pingf from './ping-func.mjs';
+
 class Ping extends EventEmitter {
   constructor(options) {
     super();
@@ -104,7 +106,12 @@ class Ping extends EventEmitter {
   async ready() {
     try {
       if (this.options.dnsResolve) {
-        await this.dnsResolve();
+        await this.dnsResolve().catch((error) => {
+          throw error;
+        });
+      } else {
+        this.result.ip = this.options.host;
+        this.result.ips = [this.result.ip];
       }
       if (this.options.filterBogon) {
         this.filterBogon();
@@ -144,86 +151,76 @@ class PingTCP extends Ping {
         : 'unknown';
     this.result.banner = '';
 
-    if (!inner) {
-      try {
-        await this.ready();
-      } catch (error) {
-        this.result.status = 'error';
-        this.result.error = error.code || error.message || error;
-        !inner ? this.emitError(error) : null;
-        throw this.result;
-      }
-    } else {
-      this.result.ip = this.options.host;
-      this.result.ips = [this.result.ip];
-      this.result.status = 'ready';
+    try {
+      await this.ready();
+    } catch (error) {
+      this.result.status = 'error';
+      this.result.error = error.code || error.message || error;
+      !inner ? this.emitError(error) : null;
+      return;
     }
 
     this.emit('ready', this.result);
 
-    return new Promise((resolve, reject) => {
-      const socket = new net.Socket();
-      const timestamp = new Date().getTime();
-      let connected = false;
+    const socket = new net.Socket();
+    const timestamp = new Date().getTime();
+    let connected = false;
 
-      socket.setTimeout(this.options.timeout);
+    socket.setTimeout(this.options.timeout);
 
-      socket.on('connect', () => {
-        connected = true;
-        this.result.ip = socket.remoteAddress;
-        this.result.time = new Date().getTime() - timestamp;
-      });
-      socket.on('data', (data) => {
-        this.result.banner += data.toString();
-        this.result.banner = this.result.banner.replace(/\r|\n/g, '');
-      });
-      socket.on('close', (error) => {
-        if (!error && this.result.status == 'ready') {
-          this.result.status = 'open';
-        }
-
-        if (this.result.banner.match(/ssh/i)) {
-          this.result.name = 'ssh';
-        } else if (this.result.banner.match(/mysql|mariadb/i)) {
-          this.result.name = 'mysql';
-        }
-
-        socket.destroy();
-
-        !inner ? this.emitResult() : null;
-        resolve(this.result);
-        return;
-      });
-      socket.on('error', (error) => {
-        this.result.time = new Date().getTime() - timestamp;
-        if (error.code == 'ECONNREFUSED') {
-          this.result.status = 'close';
-          socket.destroy();
-        } else if (error.code == 'ECONNRESET') {
-          this.result.status = 'reset';
-          socket.destroy();
-        } else {
-          this.result.status = 'error';
-          this.result.error = error.code;
-
-          socket.destroy();
-
-          !inner ? this.emitError(error) : null;
-          reject(this.result);
-          return;
-        }
-      });
-      socket.on('timeout', () => {
-        if (!connected && this.result.status == 'ready') {
-          this.result.time = new Date().getTime() - timestamp;
-          this.result.status = 'filtered';
-        }
-
-        socket.destroy();
-      });
-
-      socket.connect(this.result.port, this.result.ip);
+    socket.on('connect', () => {
+      connected = true;
+      this.result.ip = socket.remoteAddress;
+      this.result.time = new Date().getTime() - timestamp;
     });
+    socket.on('data', (data) => {
+      this.result.banner += data.toString();
+      this.result.banner = this.result.banner.replace(/\r|\n/g, '');
+    });
+    socket.on('close', (error) => {
+      if (!error && this.result.status == 'ready') {
+        this.result.status = 'open';
+      }
+
+      if (this.result.banner.match(/ssh/i)) {
+        this.result.name = 'ssh';
+      } else if (this.result.banner.match(/mysql|mariadb/i)) {
+        this.result.name = 'mysql';
+      }
+
+      socket.destroy();
+
+      !inner ? this.emitResult() : null;
+      return;
+    });
+    socket.on('error', (error) => {
+      this.result.time = new Date().getTime() - timestamp;
+      if (error.code == 'ECONNREFUSED') {
+        this.result.status = 'close';
+        socket.destroy();
+      } else if (error.code == 'ECONNRESET') {
+        this.result.status = 'reset';
+        socket.destroy();
+      } else {
+        this.result.status = 'error';
+        this.result.error = error.code;
+
+        socket.destroy();
+
+        !inner ? this.emitError(error) : null;
+        return;
+      }
+    });
+    socket.on('timeout', () => {
+      if (!connected && this.result.status == 'ready') {
+        this.result.time = new Date().getTime() - timestamp;
+        this.result.status = 'filtered';
+      }
+
+      socket.destroy();
+    });
+
+    socket.connect(this.result.port, this.result.ip);
   }
 
   async scan(inner) {
@@ -249,17 +246,13 @@ class PingTCP extends Ping {
     this.result.banners = {};
     this.result.errors = {};
 
-    if (!inner) {
-      try {
-        await this.ready();
-      } catch (error) {
-        this.result.status = 'error';
-        this.result.error = error.code || error.message || error;
-        !inner ? this.emitError(error) : null;
-        throw this.result;
-      }
-    } else {
-      this.result.status = 'ready';
+    try {
+      await this.ready();
+    } catch (error) {
+      this.result.status = 'error';
+      this.result.error = error.code || error.message || error;
+      !inner ? this.emitError(error) : null;
+      throw this.result;
     }
 
     const timestamp = new Date().getTime();
@@ -287,10 +280,12 @@ class PingTCP extends Ping {
       }
 
       const opts = JSON.parse(JSON.stringify(this.options));
+      delete opts.ports;
       opts.host = this.result.ip;
       opts.port = this.options.ports[i];
-      const task = new PingTCP(opts);
-      t[ti].push(task.send(true));
+      opts.dnsResolve = false;
+      opts.filterBogon = false;
+      t[ti].push(pingf.tcp(opts));
     }
 
     const pr = await Promise.all(t[ti]);
@@ -302,7 +297,7 @@ class PingTCP extends Ping {
 
     this.result.time = new Date().getTime() - timestamp;
     this.emitResult();
-    return this.result;
+    return;
   }
 }
 
@@ -324,80 +319,70 @@ class PingUDP extends Ping {
         ? knownPorts.names.udp[this.result.port]
         : 'unknown';
 
-    if (!inner) {
-      try {
-        await this.ready();
-      } catch (error) {
-        this.result.status = 'error';
-        this.result.error = error.code || error.message || error;
-        !inner ? this.emitError(error) : null;
-        throw this.result;
-      }
-    } else {
-      this.result.ip = this.options.host;
-      this.result.ips = [this.result.ip];
-      this.result.status = 'ready';
+    try {
+      await this.ready();
+    } catch (error) {
+      this.result.status = 'error';
+      this.result.error = error.code || error.message || error;
+      !inner ? this.emitError(error) : null;
+      return;
     }
 
     this.emit('ready', this.result);
 
-    return new Promise((resolve, reject) => {
-      const socket = new dgram.createSocket(
-        new IP(this.result.ip).is4 ? 'udp4' : 'udp6'
-      );
-      const timestamp = new Date().getTime();
-      const buffer = this.options.body
-        ? Buffer.from(this.options.body)
-        : Buffer.alloc(this.options.bytes);
-      let sto;
+    const socket = new dgram.createSocket(
+      new IP(this.result.ip).is4 ? 'udp4' : 'udp6'
+    );
+    const timestamp = new Date().getTime();
+    const buffer = this.options.body
+      ? Buffer.from(this.options.body)
+      : Buffer.alloc(this.options.bytes);
+    let sto;
 
-      socket.on('close', (error) => {
-        !inner ? this.emitResult() : null;
-        resolve(this.result);
+    socket.on('close', (error) => {
+      !inner ? this.emitResult() : null;
+      return;
+    });
+    socket.on('error', (error) => {
+      sto ? clearTimeout(sto) : null;
+
+      this.result.time = new Date().getTime() - timestamp;
+      if (error.code == 'ECONNREFUSED') {
+        this.result.status = 'close';
+        socket.close();
+      } else if (error.code == 'ECONNRESET') {
+        this.result.status = 'reset';
+        socket.close();
+      } else {
+        this.result.status = 'error';
+        this.result.error = error.code;
+
+        socket.close();
+
+        !inner ? this.emitError(error) : null;
         return;
-      });
-      socket.on('error', (error) => {
-        sto ? clearTimeout(sto) : null;
+      }
+    });
 
-        this.result.time = new Date().getTime() - timestamp;
-        if (error.code == 'ECONNREFUSED') {
-          this.result.status = 'close';
-          socket.close();
-        } else if (error.code == 'ECONNRESET') {
-          this.result.status = 'reset';
-          socket.close();
-        } else {
-          this.result.status = 'error';
-          this.result.error = error.code;
-
-          socket.close();
-
-          !inner ? this.emitError(error) : null;
-          reject(this.result);
-          return;
+    socket.connect(this.result.port, this.result.ip, () => {
+      socket.send(buffer, (error, bytes) => {
+        if (error) {
+          this.result.error = error;
+          result(error);
         }
-      });
 
-      socket.connect(this.result.port, this.result.ip, () => {
-        socket.send(buffer, (error, bytes) => {
-          if (error) {
-            this.result.error = error;
-            result(error);
-          }
-
-          this.result.status = 'open';
-          this.result.ip = socket.remoteAddress().address;
-          this.result.time = new Date().getTime() - timestamp;
-          sto = setTimeout(() => {
-            socket.close();
-          }, this.options.timeout);
-        });
+        this.result.status = 'open';
+        this.result.ip = socket.remoteAddress().address;
+        this.result.time = new Date().getTime() - timestamp;
+        sto = setTimeout(() => {
+          socket.close();
+        }, this.options.timeout);
       });
     });
   }
 
   async scan(inner) {
-    if (this.options.ports == '*') {
+    if (this.options.ports == '@') {
       this.options.ports = knownPorts.ports.udp;
     }
 
@@ -419,17 +404,13 @@ class PingUDP extends Ping {
     this.result.banners = {};
     this.result.errors = {};
 
-    if (!inner) {
-      try {
-        await this.ready();
-      } catch (error) {
-        this.result.status = 'error';
-        this.result.error = error.code || error.message || error;
-        !inner ? this.emitError(error) : null;
-        throw this.result;
-      }
-    } else {
-      this.result.status = 'ready';
+    try {
+      await this.ready();
+    } catch (error) {
+      this.result.status = 'error';
+      this.result.error = error.code || error.message || error;
+      !inner ? this.emitError(error) : null;
+      return;
     }
 
     const timestamp = new Date().getTime();
@@ -457,10 +438,13 @@ class PingUDP extends Ping {
       }
 
       const opts = JSON.parse(JSON.stringify(this.options));
+      delete opts.ports;
       opts.host = this.result.ip;
       opts.port = this.options.ports[i];
-      const task = new PingUDP(opts);
-      t[ti].push(task.send(true));
+      opts.dnsResolve = false;
+      opts.filterBogon = false;
+      const task = pingf.udp(opts);
+      t[ti].push(task);
     }
 
     const pr = await Promise.all(t[ti]);
@@ -472,7 +456,7 @@ class PingUDP extends Ping {
 
     this.result.time = new Date().getTime() - timestamp;
     this.emitResult();
-    return this.result;
+    return;
   }
 }
 
@@ -491,19 +475,13 @@ class PingICMP extends Ping {
     this.result.ttl = this.options.ttl;
     this.result.bytes = -1;
 
-    if (!inner) {
-      try {
-        await this.ready();
-      } catch (error) {
-        this.result.status = 'error';
-        this.result.error = error.code || error.message || error;
-        !inner ? this.emitError(error) : null;
-        throw this.result;
-      }
-    } else {
-      this.result.ip = this.options.host;
-      this.result.ips = [this.result.ip];
-      this.result.status = 'ready';
+    try {
+      await this.ready();
+    } catch (error) {
+      this.result.status = 'error';
+      this.result.error = error.code || error.message || error;
+      !inner ? this.emitError(error) : null;
+      return;
     }
 
     this.emit('ready', this.result);
@@ -515,95 +493,90 @@ class PingICMP extends Ping {
       ? raw.Protocol.ICMP
       : raw.Protocol.ICMPv6;
 
-    return new Promise((resolve, reject) => {
-      const socket = raw.createSocket({
-        addressFamily: this.addressFamily,
-        protocol: this.protocol,
-      });
-      const timestamp = new Date().getTime();
-      const body = this.#randomString(this.options.bytes - 24);
-      const buffer = this.#createBuffer('abcdefgh' || '');
-      const sto = setTimeout(() => {
-        if (this.result.status == 'ready') {
-          this.result.time = new Date().getTime() - timestamp;
-          this.result.status = 'timeout';
-          socket.close();
-        }
-      }, this.options.timeout);
+    const socket = raw.createSocket({
+      addressFamily: this.addressFamily,
+      protocol: this.protocol,
+    });
+    const timestamp = new Date().getTime();
+    const body = this.#randomString(this.options.bytes - 24);
+    const buffer = this.#createBuffer('abcdefgh' || '');
+    const sto = setTimeout(() => {
+      if (this.result.status == 'ready') {
+        this.result.time = new Date().getTime() - timestamp;
+        this.result.status = 'timeout';
+        socket.close();
+      }
+    }, this.options.timeout);
 
-      socket.on('message', (data, source) => {
-        const parsed = this.#parseBuffer(Buffer.from(data), source);
-        if (parsed == null) {
+    socket.on('message', (data, source) => {
+      const parsed = this.#parseBuffer(Buffer.from(data), source);
+      if (parsed == null) {
+        return;
+      }
+
+      clearTimeout(sto);
+
+      this.result.reply = parsed;
+
+      this.result.time = new Date().getTime() - timestamp;
+      if (this.result.status == 'ready') {
+        if (parsed.type == 0) {
+          this.result.status = 'reply';
+        } else {
+          this.result.status = 'exception';
+        }
+      }
+
+      socket.close();
+    });
+    socket.on('close', () => {
+      !inner ? this.emitResult() : null;
+      return;
+    });
+    socket.on('error', (err) => {
+      console.log(err);
+      clearTimeout(sto);
+
+      this.result.error = err.code || err.message || err;
+      this.result.status = 'error';
+
+      socket.close();
+
+      !inner ? this.emitError(err) : null;
+      return;
+    });
+
+    socket.send(
+      buffer,
+      0,
+      buffer.length,
+      this.result.ip,
+      () => {
+        socket.setOption(
+          new IP(this.result.ip).is4
+            ? raw.SocketLevel.IPPROTO_IP
+            : raw.SocketLevel.IPPROTO_IPV6,
+          new IP(this.result.ip).is4
+            ? raw.SocketOption.IP_TTL
+            : raw.SocketOption.IPV6_TTL,
+          this.options.ttl
+        );
+      },
+      (error, bytes) => {
+        this.result.bytes = bytes;
+        if (error) {
+          clearTimeout(sto);
+
+          this.result.error = error.code || error.message || error;
+          this.result.status = 'error';
+
+          socket.close();
+
+          !inner ? this.emitError(error) : null;
           return;
         }
-
-        clearTimeout(sto);
-
-        this.result.reply = parsed;
-
-        this.result.time = new Date().getTime() - timestamp;
-        if (this.result.status == 'ready') {
-          if (parsed.type == 0) {
-            this.result.status = 'reply';
-          } else {
-            this.result.status = 'exception';
-          }
-        }
-
-        socket.close();
-      });
-      socket.on('close', () => {
-        !inner ? this.emitResult() : null;
-        resolve(this.result);
-        return;
-      });
-      socket.on('error', (err) => {
-        console.log(err);
-        clearTimeout(sto);
-
-        this.result.error = err.code || err.message || err;
-        this.result.status = 'error';
-
-        socket.close();
-
-        !inner ? this.emitError(err) : null;
-        reject(this.result);
-        return;
-      });
-
-      socket.send(
-        buffer,
-        0,
-        buffer.length,
-        this.result.ip,
-        () => {
-          socket.setOption(
-            new IP(this.result.ip).is4
-              ? raw.SocketLevel.IPPROTO_IP
-              : raw.SocketLevel.IPPROTO_IPV6,
-            new IP(this.result.ip).is4
-              ? raw.SocketOption.IP_TTL
-              : raw.SocketOption.IPV6_TTL,
-            this.options.ttl
-          );
-        },
-        (error, bytes) => {
-          this.result.bytes = bytes;
-          if (error) {
-            clearTimeout(sto);
-
-            this.result.error = error.code || error.message || error;
-            this.result.status = 'error';
-
-            socket.close();
-
-            !inner ? this.emitError(error) : null;
-            reject(this.result);
-            return;
-          }
-        }
-      );
-    });
+      }
+    );
   }
 
   #parseBuffer(buf, src) {
@@ -731,17 +704,13 @@ class PingICMP extends Ping {
       Math.min(256, this.options.timeoutx ? this.options.timeoutx * 1 : 8)
     );
 
-    if (!inner) {
-      try {
-        await this.ready();
-      } catch (error) {
-        this.result.status = 'error';
-        this.result.error = error.code || error.message || error;
-        !inner ? this.emitError(error) : null;
-        throw this.result;
-      }
-    } else {
-      this.result.status = 'ready';
+    try {
+      await this.ready();
+    } catch (error) {
+      this.result.status = 'error';
+      this.result.error = error.code || error.message || error;
+      !inner ? this.emitError(error) : null;
+      return;
     }
 
     const timestamp = new Date().getTime();
@@ -749,8 +718,11 @@ class PingICMP extends Ping {
     let timeoutstack = 0;
     for (let i = this.options.ttln; i <= this.options.ttlx; i++) {
       const opts = JSON.parse(JSON.stringify(this.options));
+      opts.host = this.result.ip;
       opts.ttl = i;
-      const result = await new PingICMP(opts).send();
+      opts.dnsResolve = false;
+      opts.filterBogon = false;
+      const result = await pingf.icmp(opts);
 
       const hop = {
         status: result.status,
@@ -783,7 +755,7 @@ class PingICMP extends Ping {
 
     this.result.time = new Date().getTime() - timestamp;
     this.emitResult();
-    return this.result;
+    return;
   }
 }
 
