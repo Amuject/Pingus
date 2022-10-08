299dc539724d05cf6ea1fb3d4eae0701184cb138
Wany <sung@wany.io>
2022-10-08 18:11:01 +0900

    add ports option can parse string

--- example/test.mjs
+++ example/test.mjs
@@ -1,10 +1,1 @@
 import pingus from 'pingus';
-
-// UDP ping scan to localhost
-pingus
-  .tcpscan({
-    host: 'server-pingo.wnynya.com',
-    timeout: 500,
-    ports: '20-24,80,443',
-  })
-  .then(console.log);
